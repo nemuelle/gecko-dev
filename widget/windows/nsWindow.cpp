a6ac144c0f131ae476c7da676e85762e48e9ce31
thomasmo <thomasmo@mozilla.com>
2020-02-19 19:12:22 -0600

    Stage 1.4: Create a thread to take Overlay input
    This change introduces a new thread that polls OpenVR for messages from the created overlay.
    This is validated in this change via log messages in debug builds, accessed from the command line with
        --MOZ_LOG=FxRWindowManager:5

--- widget/windows/nsWindow.cpp
+++ widget/windows/nsWindow.cpp
@@ -212,6 +212,7 @@
 #include "mozilla/Telemetry.h"
 #include "mozilla/plugins/PluginProcessParent.h"
 #include "mozilla/webrender/WebRenderAPI.h"
+#include "FxRWindowManager.h"
 
 using namespace mozilla;
 using namespace mozilla::dom;
@@ -965,6 +966,13 @@ void nsWindow::Destroy() {
     mWindowHook.Notify(mWnd, WM_DESTROY, 0, 0, msgResult);
     OnDestroy();
   }
+
+  if (mFxrOverlayId > 0) {
+    FxRWindowManager::GetInstance()->RemoveWindow(mFxrOverlayId);
+
+    mFxrOverlayId = 0;
+  }
+  
 }
 
 /**************************************************************
