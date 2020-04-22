1872a9d2789141be0ee2c3f797767cc53fbeb38d
thomasmo <thomasmo@mozilla.com>
2020-04-22 16:15:17 -0500

    Stage 3g: Add play/pause functionality to transport controls
    This change introduces plumbing for new APIs for chrome JS to interact with FxRWindowManager, allowing for changes to playback state of fullscreen media and for changing projection mode for 360 video.
    In this change, the play/pause button now works, and the other buttons simply output to the console to demonstrate the API calls.

--- dom/base/ChromeUtils.cpp
+++ dom/base/ChromeUtils.cpp
@@ -49,6 +49,7 @@
 #  include "ProfilerMarkerPayload.h"
 #endif
 #include "nsIException.h"
+#include "FxRWindowManager.h"
 
 namespace mozilla {
 namespace dom {
@@ -1285,6 +1286,18 @@ MediaSessionPlaybackTestState ChromeUtils::GetCurrentMediaSessionPlaybackState(
         service->GetMainControllerPlaybackState());
   }
   return MediaSessionPlaybackTestState::Stopped;
+}
+
+/* static */
+void ChromeUtils::SetFxrPlayMediaState(const GlobalObject& aGlobal,
+  const nsAString& aState) {
+  FxRWindowManager::GetInstance()->SetPlayMediaState(aState);
+}
+
+/* static */
+void ChromeUtils::SetFxrProjectionMode(const GlobalObject& aGlobal,
+  const nsAString& aMode) {
+  FxRWindowManager::GetInstance()->SetProjectionMode(aMode);
 }
 
 }  // namespace dom
