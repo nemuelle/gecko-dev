8e1f95d733683ce4065516582eab71b7cc0ed3eb
thomasmo <thomasmo@mozilla.com>
2020-04-23 14:27:56 -0500

    Stage 3h: Create Overlay for Transport controls
    This change creates a new OpenVR overlay to render the transport control window separately from the video content. Several parts of FxRWindowManager is refactored to reuse some routines performed for the main window.

--- gfx/vr/FxRWindowManager.h
+++ gfx/vr/FxRWindowManager.h
@@ -40,6 +40,30 @@ enum FxRProjectionMode {
 // the top-level windows created for Firefox Reality on Desktop. Only a
 // single window is initially supported.
 class FxRWindowManager final {
+ // TODO: make this a class, and move methods from the manager that only
+ // interact with struct members to this class (i.e., some of the static
+ // functions that take an FxRWindow
+ private:
+  struct FxRWindow {
+    // Note: mWidget takes a full reference
+    nsIWidget* mWidget;
+    nsPIDOMWindowOuter* mWindow;
+    vr::VROverlayHandle_t mOverlayHandle;
+    vr::VROverlayHandle_t mOverlayThumbnailHandle;
+    HWND mHwndWidget;
+
+    // Works with Collect/ProcessOverlayEvents to transfer OpenVR input events
+    // from the background thread to the main thread. Consider using a queue?
+    VREventVector mEventsVector;
+    CRITICAL_SECTION mEventsCritSec;
+
+    // OpenVR scroll event doesn't provide the position of the controller on the
+    // overlay, so keep track of the last-known position to use with the scroll
+    // event
+    POINT mLastMousePt;
+    RECT mOverlaySizeRec;
+  };
+
  public:
   static FxRWindowManager* GetInstance();
   ~FxRWindowManager();
@@ -52,10 +76,8 @@ class FxRWindowManager final {
 
   bool AddWindow(nsPIDOMWindowOuter* aWindow);
   void RemoveWindow(uint64_t aOverlayId);
-
   bool IsFxRWindow(uint64_t aOuterWindowID);
   bool IsFxRWindow(const nsWindow* aWindow) const;
-  uint64_t GetWindowID() const;
 
   void ShowVirtualKeyboard(uint64_t aOverlayId);
   void HideVirtualKeyboard();
@@ -72,9 +94,18 @@ class FxRWindowManager final {
   vr::VROverlayError ChangeProjectionMode(FxRProjectionMode projectionMode);
 
  private:
+  static void InitWindow(FxRWindow& newWindow, nsPIDOMWindowOuter* aWindow);
+  static void CleanupWindow(FxRWindow& fxrWindow);
+  FxRWindow& GetFxrWindowFromWidget(nsIWidget* widget);  
+  bool CreateOverlayForWindow(FxRWindow& newWindow, char* name, float width);
+
   vr::VROverlayError SetupOverlayInput(vr::VROverlayHandle_t overlayId);
   static DWORD OverlayInputPump(_In_ LPVOID lpParameter);
-  void CollectOverlayEvents();
+  void CollectOverlayEvents(FxRWindow& fxrWindow);
+
+  void ToggleMedia();
+  void EnsureTransportControls();
+  void HideTransportControls();
 
   FxRWindowManager();
 
@@ -84,6 +115,7 @@ class FxRWindowManager final {
   vr::IVRSystem* mVrApp;
   int32_t mDxgiAdapterIndex;
 
+  // Members for Input
   mozilla::Atomic<bool> mIsOverlayPumpActive;
   HANDLE mOverlayPumpThread;
 
@@ -94,25 +126,6 @@ class FxRWindowManager final {
   // Only a single window is supported for tracking. Support for multiple
   // windows will require a data structure to collect windows as they are
   // created.
-  struct FxRWindow {
-    // Note: mWidget takes a full reference
-    nsIWidget* mWidget;
-    nsPIDOMWindowOuter* mWindow;
-    vr::VROverlayHandle_t mOverlayHandle;
-	vr::VROverlayHandle_t mTransportControlsOverlayHandle;
-	vr::VROverlayHandle_t mOverlayThumbnailHandle;
-    HWND mHwndWidget;
-
-    // Works with Collect/ProcessOverlayEvents to transfer OpenVR input events
-    // from the background thread to the main thread. Consider using a queue?
-    VREventVector mEventsVector;
-    CRITICAL_SECTION mEventsCritSec;
-
-    // OpenVR scroll event doesn't provide the position of the controller on the
-    // overlay, so keep track of the last-known position to use with the scroll
-    // event
-    POINT mLastMousePt;
-    RECT mOverlaySizeRec;
-  } mFxRWindow;
-  // nsPIDOMWindowOuter* mWindow;
+  FxRWindow mFxRWindow;
+  FxRWindow mTransportWindow;
 };
--- gfx/vr/nsFxrCommandLineHandler.cpp
+++ gfx/vr/nsFxrCommandLineHandler.cpp
@@ -94,7 +94,6 @@ nsFxrCommandLineHandler::Handle(nsICommandLine* aCmdLine) {
     MOZ_ASSERT(result == NS_OK);
 
     if (FxRWindowManager::GetInstance()->VRinit()) {
-
       nsPIDOMWindowOuter* newWindowOuter = nsPIDOMWindowOuter::From(newWindow);
       if (FxRWindowManager::GetInstance()->AddWindow(newWindowOuter)) {
         // Set ForceFullScreenInWidget so that full-screen (in an FxR window)
@@ -102,13 +101,6 @@ nsFxrCommandLineHandler::Handle(nsICommandLine* aCmdLine) {
         // shared with the host. Also, this is set here per-window because
         // changing the related pref would impact all browser window instances.
         newWindowOuter->ForceFullScreenInWidget();
-
-
-        // Associate this new window with this new OpenVR overlay for output
-        // rendering
-        nsCOMPtr<nsIWidget> newWidget =
-          mozilla::widget::WidgetUtils::DOMWindowToWidget(newWindowOuter);
-        newWidget->RequestFxrOutput(FxRWindowManager::GetInstance()->GetOverlayId());
       }
       else {
         MOZ_ASSERT(false, "Failed to create Overlay for FxR");
