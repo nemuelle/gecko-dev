27ccbcad8fff1c2c62003cf01ad8c03587ddcb43
thomasmo <thomasmo@mozilla.com>
2020-08-03 10:02:11 -0500

    BUG: Update feedback link

--- config/makefiles/rust.mk
+++ config/makefiles/rust.mk
@@ -63,6 +63,11 @@ ifndef MOZ_DEBUG_RUST
 ifeq (,$(findstring gkrust_gtest,$(RUST_LIBRARY_FILE)))
 cargo_rustc_flags += -Clto
 endif
+# Versions of rust >= 1.45 need -Cembed-bitcode=yes for all crates when
+# using -Clto.
+ifeq (,$(filter 1.38.% 1.39.% 1.40.% 1.41.% 1.42.% 1.43.% 1.44.%,$(RUSTC_VERSION)))
+RUSTFLAGS += -Cembed-bitcode=yes
+endif
 endif
 endif
 
